d8605324f5b5af956472514eaf9fb6792ad23b6b
lenonfelippi <lenonfelippi1@gmail.com>
2021-05-16 00:26:38 -0300

    configura next-pwa e seu manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next :D - Boilerplate</title>
+        <title>Boilerplate NextJS</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#07556d" />
         <meta
           name="description"
           content="A simple projext starter to work with TypeScript, React, NextJS and Styled Components"
